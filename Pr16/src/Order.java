bb62d88de3981909334dd3b4bd8018313b0090a4
uulyanamuur <57221333+uulyanamuur@users.noreply.github.com>
2021-01-17 23:57:42 +0300

    Add files via upload

---
+++ Pr16/src/Order.java
@@ -0,0 +1,11 @@
+public interface Order {
+    public boolean add(Item item);
+    public boolean del(String itemName);
+    public int delAll(String itemName);
+    public int itemQuantity();
+    public Item[] getItems();
+    public double getTotalCost();
+    public int getQuantityByName(String name);
+    public String[] getItemsNames();
+    public Item[] getSortItems();
+}
